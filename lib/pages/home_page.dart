55912fbf1387f09c7c2991dd70be0e1fbec2401b
piro-piyush <piyush72717272@gmail.com>
2024-11-02 10:51:06 +0530

    implementing search

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   String searchQuery = "";
-
+  
 
   @override
   Widget build(BuildContext context) {
@@ -29,41 +29,61 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: notesProvider.isLoading == false? SafeArea(child:(notesProvider.notes.isEmpty)? Center(
-          child: Text("No notes yet")): GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemCount: notesProvider.notes.length,
-          itemBuilder: (context, index){
-            Note currentNote = notesProvider.notes[index];
-            return GestureDetector(
-              onTap: (){
-                Navigator.push(context, CupertinoPageRoute(
-                    fullscreenDialog: true,
-                    builder: (context) => AddNewNotePage(isUpdate: true,note: currentNote,)),
-                );
-              },
-              onLongPress: (){
-                notesProvider.deleteNote(currentNote);
-              },
-              child: Container(
-                margin:EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  // color:  Color(0xFF9FC8FB),
-                      borderRadius: BorderRadius.circular(14),border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
-                )),
-                child: Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(currentNote.title!,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),maxLines: 1,overflow: TextOverflow.ellipsis,),
-                      SizedBox(height: 7,),
-                      Text(currentNote.content!,style: TextStyle(fontSize: 18,color: Colors.grey[700]),maxLines: 4,overflow: TextOverflow.ellipsis,),
-                  ]),
+          child: Text("No notes yet")): 
+          ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (val) {
+                    setState(() {
+                      searchQuery = val;
+                    });
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Search"
+                  ),
                 ),
               ),
-            );
-          })):Center(child: CircularProgressIndicator()),
+              (notesProvider.getFilteredNotes(searchQuery).length > 0) ?GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                  itemCount: notesProvider.getFilteredNotes(searchQuery).length,
+                  itemBuilder: (context, index) {
+                  Note currentNote = notesProvider.getFilteredNotes(searchQuery)[index];
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) => AddNewNotePage(isUpdate: true,note: currentNote,)),
+                      );
+                    },
+                    onLongPress: (){
+                      notesProvider.deleteNote(currentNote);
+                    },
+                    child: Container(
+                      margin:EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        // color:  Color(0xFF9FC8FB),
+                          borderRadius: BorderRadius.circular(14),border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      )),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(currentNote.title!,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),maxLines: 1,overflow: TextOverflow.ellipsis,),
+                              SizedBox(height: 7,),
+                              Text(currentNote.content!,style: TextStyle(fontSize: 18,color: Colors.grey[700]),maxLines: 4,overflow: TextOverflow.ellipsis,),
+                            ]),
+                      ),
+                    ),
+                  );
+                }):Padding(
+                padding: EdgeInsets.all(20),
+                child: Text("No notes found!", textAlign: TextAlign.center,),
+              ),]
+          )):Center(child: CircularProgressIndicator()),
       floatingActionButton: FloatingActionButton(onPressed: (){
         Navigator.push(context, CupertinoPageRoute(
             fullscreenDialog: true,
